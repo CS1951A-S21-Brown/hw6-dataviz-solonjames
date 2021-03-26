1d5de1a0e7fa9ce4d3aef43defee982f5d4f7f14
Solon James <solon_james@brown.edu>
2021-03-25 21:55:34 -0400

    done with graphs just text left

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const MAX_HEIGHT = 720;
 const margin = { top: 40, right: 100, bottom: 40, left: 175 };
 
 // Assumes the same graph width, height dimensions as the example dashboard. Feel free to change these if you'd like
-let graph_1_width = MAX_WIDTH, graph_1_height = 400;
-let graph_2_width = MAX_WIDTH, graph_2_height = 400;
-let graph_3_width = MAX_WIDTH, graph_3_height = 400;
+let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 250;
+let graph_2_width = (MAX_WIDTH / 2) - 10, graph_2_height = 275;
+let graph_3_width = MAX_WIDTH / 2, graph_3_height = 575;
 
 //graph 1
 
@@ -62,7 +62,6 @@ function setData(allTime) {
         x.domain([0, d3.max(data, (d) => parseFloat(d.Global_Sales))]);
 
         //set y domain
-        console.log(data.map(x => x.Name + ": " + x.Platform));
         y.domain(data.map(x => x.Name + ": " + x.Platform));
 
         y_axis_label.call(d3.axisLeft(y).tickSize(0).tickPadding(10));
@@ -71,7 +70,7 @@ function setData(allTime) {
 
         let color = d3.scaleOrdinal()
             .domain(data.map(function (d) { return d.Name + ": " + d.Platform }))
-            .range(d3.quantize(d3.interpolateHcl("#66a0e2", "#81c2c3"), 10));
+            .range(d3.quantize(d3.interpolateHcl("#D36135", "#3E5641"), 10));
 
         bars.enter()
             .append("rect")
@@ -112,13 +111,19 @@ let svg2 = d3.select("body")
     .attr("height", graph_2_height)
 
 let title2 = svg2.append("text")
-    .attr("transform", "translate(250, 30)")
+    .attr("transform", `translate(${graph_2_width / 2}, 30)`)
     .style("text-anchor", "middle")
     .style("font-size", 15)
     .text("Best Selling Video Game Genere by Region")
 
+let legend2 = svg2.append("text")
+    .attr("transform", `translate(${graph_2_width / 2}, 270)`)
+    .style("text-anchor", "middle")
+    .style("font-size", 15)
+    .text("Red: Action, Blue: Role-Playing")
+
 var projection = d3.geoNaturalEarth()
-    .scale((graph_2_width - 750) / 1.3 / Math.PI)
+    .scale(graph_2_width / 3.2 / Math.PI)
     .translate([graph_2_width / 2, graph_2_height / 2]);
 
 
@@ -135,8 +140,6 @@ Promise.all([d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/
         "Action": "#f54242",
         "Role-Playing": "#4287f5"
     }
-    console.log(getMaxGenre(data2, "JP"));
-    console.log(genreToColorMap[getMaxGenre(data2, "JP")]);
     const beepBoop100k = svg2.append("g")
         .selectAll("path");
     const colorRegion = (region, color) => beepBoop100k
@@ -154,45 +157,46 @@ Promise.all([d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/
 });
 
 
-// graph 3
+//graph 3
 let svg3 = d3.select("body")
     .append("svg")
-    .attr("width", graph_1_width)
-    .attr("height", graph_1_height)
+    .attr("width", graph_3_width)
+    .attr("height", graph_3_height + 150)
     .append("g")
-    .attr("transform", "translate(200, 40)");
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 var x3 = d3.scaleLinear()
-    .range([40, 400]);
+    .range([0, graph_3_width - margin.left - margin.right]);
 
 var y3 = d3.scaleBand()
-    .range([50, 500])
+    .range([margin.top, graph_3_height - margin.top - margin.bottom])
     .padding(0.1);
 
-let countRef3 = svg.append("g");
-let y_axis_label3 = svg.append("g");
+let countRef3 = svg3.append("g");
+let y_axis_label3 = svg3.append("g");
 
 //set up x label
 svg3.append("text")
-    .attr("transform", "translate(100, 200)")
+    .attr("transform", "translate(150, 550)")
     .style("text-anchor", "middle")
     .text("Global Sales");
 
 //set up y label
 let y_axis_text3 = svg3.append("text")
-    .attr("transform", "translate(-90, 15)")
+    .attr("transform", "translate(-75, 15)")
     .style("text-anchor", "middle")
-    .text("Game Name");
+    .text("Game Category")
 
 //set up title
 let title3 = svg3.append("text")
     .attr("transform", "translate(150, 15)")
     .style("text-anchor", "middle")
-    .style("font-size", 15);
+    .style("font-size", 15)
+    .text("Publisher Sales by Category")
 
 d3.csv("../data/video_games.csv").then(function (data3) {
+    notRolled = data3
     data3 = cleanDataByGenre(data3);
-    console.log(data3)
 
     x3.domain([0, d3.max(data3, ([, d]) => parseFloat(d))]);
 
@@ -200,25 +204,50 @@ d3.csv("../data/video_games.csv").then(function (data3) {
     data3.forEach((y, x) => genreList.add(x.split("///")[0]));
 
     const publisherList = new Set();
-    data3.forEach((y, x) => genreList.add(x.split("///")[1]));
+    data3.forEach((y, x) => publisherList.add(x.split("///")[1]));
 
     let colorPublisher = d3.scaleOrdinal()
         .domain([...publisherList])
         .range(d3.quantize(d3.interpolateHcl("#66a0e2", "#81c2c3"), 100));
 
     y3.domain([...genreList]);
+    y_axis_label3.call(d3.axisLeft(y3).tickSize(0).tickPadding(10));
 
-    console.log(y3("Sports"))
+    svg3.append("g")
+        .attr("transform", `translate(0,${graph_3_height - margin.top - margin.bottom})`)
+        .call(d3.axisBottom(x3));
 
-    svg3.append('g')
+    const div = d3.select("body").append("div")
+        .attr("class", "yeet")
+        .style("opacity", 0);
+
+    const dots = svg3.append('g')
         .selectAll("dot")
         .data(data3)
         .enter()
         .append("circle")
         .attr("cx", function ([, d]) { return x3(parseFloat(d)); })
-        .attr("cy", function ([d,]) { return (y3(d.split("///")[0]) - 50) * 300 / 11 })
+        .attr("cy", function ([d,]) { return (y3(d.split("///")[0]) + 17) })
         .attr("r", 4)
         .style("fill", function ([d,]) { return colorPublisher(d.split("///")[1]) })
+        .on("mouseover", function (d, datum) {
+            var matching = notRolled.filter((x) => x.Genre == datum[0].split("///")[0] && x.Publisher == datum[0].split("///")[1])
+            var notMatching = notRolled.filter((x) => x.Genre == datum[0].split("///")[0] && x.Publisher != datum[0].split("///")[1])
+            matching = matching.map((x) => parseFloat(x.Global_Sales))
+            notMatching = notMatching.map((x) => parseFloat(x.Global_Sales))
+            div.transition()
+                .duration(200)
+                .style("opacity", 1);
+            div.html(`Company Name : ${datum[0].split("///")[1]}` + "<br/>" + `p Value : ${get_t_test(notMatching, matching)}`)
+                .style("left", (d.pageX) + "px")
+                .style("top", (d.pageY - 28) + "px");
+        })
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", 0);
+        });
+
 
 });
 
@@ -234,4 +263,13 @@ function cleanDataByYear(data, allTime, yearInterested) {
 
 function cleanDataByGenre(data) {
     return d3.rollup(data, v => d3.sum(v, d => d["Global_Sales"]), d => (d.Genre + "///" + d.Publisher));
+}
+
+function get_t_test(sample1, sample2) {
+    avg1 = jStat.mean(sample1);
+    avg2 = jStat.mean(sample2);
+    sum_Squared = (jStat.sum(jStat.pow(jStat.subtract(sample1, avg1), 2)) + jStat.sum(jStat.pow(jStat.subtract(sample2, avg2), 2))) / (sample1.length + sample2.length - 2);
+    score = (avg1 - avg2) / Math.sqrt(sum_Squared / sample1.length + sum_Squared / sample2.length);
+    val = jStat.studentt.cdf(-Math.abs(score), sample1.length + sample2.length - 2) * 2;
+    return val;
 }
